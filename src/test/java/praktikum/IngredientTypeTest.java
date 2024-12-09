30a3d267f94df338d23d431ce598a8a372b77eeb
kirvasqa <kirvasqa@gmail.com>
2024-12-09 08:24:19 +0300

    added Test in IngredientTypeTest

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -1,7 +1,31 @@
 package praktikum;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class IngredientTypeTest {
 
+    @Parameterized.Parameter(0)
+    public String name;
+
+    @Parameterized.Parameter(1)
+    public IngredientType expectedType;
+
+    @Parameterized.Parameters
+    public static Object[][] data() {
+        return new Object[][] {
+                {"SAUCE", IngredientType.SAUCE},
+                {"FILLING", IngredientType.FILLING}
+        };
+    }
+
+    @Test
+    public void testIngredientTypeValueOf() {
+        assertEquals(expectedType, IngredientType.valueOf(name));
+    }
 }
